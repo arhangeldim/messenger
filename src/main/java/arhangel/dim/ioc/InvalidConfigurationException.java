0cb63bc13605df773a2c23d500c78e703ec7070a
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2016-03-06 14:21:46 +0300

    Graph demo

---
+++ src/main/java/arhangel/dim/ioc/InvalidConfigurationException.java
@@ -0,0 +1,10 @@
+package arhangel.dim.ioc;
+
+/**
+ *
+ */
+public class InvalidConfigurationException extends Exception {
+    public InvalidConfigurationException(String msg) {
+        super(msg);
+    }
+}
