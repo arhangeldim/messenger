29fe1914bbb24efa09ada79cb3db003e30932707
BaranovAV <andy-92@mail.ru>
2016-03-15 14:46:17 +0300

    added xmlBeanReader class

---
+++ src/main/java/arhangel/dim/container/BeanXmlReader.java
@@ -0,0 +1,20 @@
+package arhangel.dim.container;
+
+import java.beans.XMLDecoder;
+
+/**
+ * Created by andy on 14.03.2016.
+ */
+public class BeanXmlReader {
+    private static final String TAG_BEAN = "bean";
+    private static final String TAG_PROPERTY = "property";
+    private static final String ATTR_NAME = "name";
+    private static final String ATTR_VALUE = "val";
+    private static final String ATTR_REF = "ref";
+    private static final String ATTR_BEAN_ID = "id";
+    private static final String ATTR_BEAN_CLASS = "class";
+
+    public List<Bean> parseBeans(String pathToFile) {
+        ..
+    }
+}
