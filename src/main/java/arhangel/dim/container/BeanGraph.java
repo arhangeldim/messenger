1c389597829dc31e42597a9c975fd65c4511d748
BaranovAV <andy-92@mail.ru>
2016-03-15 16:14:37 +0300

    solution of point 2

--- src/main/java/arhangel/dim/container/BeanGraph.java
+++ src/main/java/arhangel/dim/container/BeanGraph.java
@@ -1,8 +1,12 @@
 package arhangel.dim.container;
 
+import sun.security.provider.certpath.Vertex;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
+import java.util.ArrayList;
 
 /**
  *
@@ -10,41 +14,71 @@ import java.util.Map;
 public class BeanGraph {
     // Граф представлен в виде списка связности для каждой вершины
     private Map<BeanVertex, List<BeanVertex>> vertices = new HashMap<>();
+    private Stack<BeanVertex> stackBean = new Stack<>();
+//    private Stack<BeanVertex> stackBean = new Stack<>();
 
     /**
      * Добавить вершину в граф
+     *
      * @param value - объект, привязанный к вершине
      */
     public BeanVertex addVertex(Bean value) {
-        return null;
+        BeanVertex vertex = new BeanVertex(value);
+        this.vertices.put(vertex, new ArrayList<>());
+        return vertex;
     }
 
     /**
      * Соединить вершины ребром
+     *
      * @param from из какой вершины
-     * @param to в какую вершину
+     * @param to   в какую вершину
      */
-    public void addEdge(BeanVertex from ,BeanVertex to) {
+    public void addEdge(BeanVertex from, BeanVertex to) {
+        this.vertices.get(from).add(to);
     }
 
     /**
      * Проверяем, связаны ли вершины
      */
     public boolean isConnected(BeanVertex v1, BeanVertex v2) {
-        return false;
+        return this.vertices.containsKey(v1) && this.vertices.get(v1).contains(v2);
     }
 
     /**
      * Получить список вершин, с которыми связана vertex
      */
     public List<BeanVertex> getLinked(BeanVertex vertex) {
-        return null;
+        return this.vertices.get(vertex);
     }
 
     /**
      * Количество вершин в графе
      */
     public int size() {
-        return 0;
+        if (this.vertices.isEmpty()) {
+            return 0;
+        }
+        return this.vertices.size();
+    }
+
+    public List<BeanVertex> sort() {
+        List<BeanVertex> sortedBeanVertexes = new ArrayList<>();
+        for (BeanVertex v : this.vertices.keySet()) {
+            this.sortStage(v);
+        }
+        while (!this.stackBean.isEmpty()) {
+            sortedBeanVertexes.add(this.stackBean.pop());
+        }
+        return sortedBeanVertexes;
+    }
+
+    private void sortStage(BeanVertex vertex) {
+        if (!this.vertices.get(vertex).isEmpty()) {
+            for (BeanVertex v : this.vertices.get(vertex)) {
+                this.sortStage(v);
+            }
+        }
+        this.stackBean.push(vertex);
     }
 }
--- src/main/java/arhangel/dim/container/BeanXmlReader.java
+++ src/main/java/arhangel/dim/container/BeanXmlReader.java
@@ -1,6 +1,15 @@
 package arhangel.dim.container;
 
-import java.beans.XMLDecoder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * Created by andy on 14.03.2016.
@@ -14,7 +23,49 @@ public class BeanXmlReader {
     private static final String ATTR_BEAN_ID = "id";
     private static final String ATTR_BEAN_CLASS = "class";
 
-    public List<Bean> parseBeans(String pathToFile) {
-        ..
+    public List<Bean> parseBeans(String pathToFile) throws Exception {
+        Document doc = this.getDocumentObject(pathToFile);
+        NodeList beanNodeList = doc.getElementsByTagName(TAG_BEAN);
+        List<Bean> beansList = new LinkedList<>();
+        for (int i = 0; i < beanNodeList.getLength(); i++) {
+            Element currentBean = (Element) beanNodeList.item(i);
+            Map<String, Property> beanAttributes = this.getAttributeMapForBean(currentBean);
+            String beanName = currentBean.getAttribute(ATTR_BEAN_ID);
+            String beanClass = currentBean.getAttribute(ATTR_BEAN_CLASS);
+            beansList.add(
+                    new Bean(
+                            beanName,
+                            beanClass,
+                            beanAttributes
+                    )
+            );
+        }
+        return beansList;
+    }
+
+    private Document getDocumentObject(String pathToFile) throws Exception {
+        File xmlFile = new File(pathToFile);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dbuilder = dbFactory.newDocumentBuilder();
+        Document doc = dbuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize();
+        return doc;
+    }
+
+    private Map<String, Property> getAttributeMapForBean(Element element) {
+        Map<String, Property> beanAttributes = new HashMap<>();
+        NodeList beanAttributeList = element.getElementsByTagName(TAG_PROPERTY);
+        for (int j = 0; j < beanAttributeList.getLength(); j++) {
+            Element attribute = (Element) beanAttributeList.item(j);
+            Property property = null;
+            String name = attribute.getAttribute(ATTR_NAME);
+            if (attribute.hasAttribute(ATTR_REF)) {
+                property = new Property(name,attribute.getAttribute(ATTR_REF),ValueType.REF);
+            } else if (attribute.hasAttribute(ATTR_VALUE)) {
+                property = new Property(name,attribute.getAttribute(ATTR_VALUE),ValueType.VAL);
+            }
+            beanAttributes.put(name,property);
+        }
+        return beanAttributes;
     }
 }
