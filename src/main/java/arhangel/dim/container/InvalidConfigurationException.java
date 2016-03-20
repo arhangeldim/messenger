af6f570d6a9c5b0ddc2c8692de8cf88e3b882fc4
Vitaly Malygin <vitalikmalygin@gmail.com>
2016-03-21 02:04:42 +0300

    made checkstyle 2

--- src/main/java/arhangel/dim/container/InvalidConfigurationException.java
+++ src/main/java/arhangel/dim/container/InvalidConfigurationException.java
@@ -4,5 +4,7 @@ package arhangel.dim.container;
  * Created by vital on 19.03.16.
  */
 public class InvalidConfigurationException extends Exception {
-    public InvalidConfigurationException(String message) { super(message);}
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
 }
