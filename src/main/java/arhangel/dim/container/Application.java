9dfdae0f41f8a13a6788ac502be25bede99c84a4
spec45as <spec45as@gmail.com>
2016-03-23 02:27:35 +0300

    Задание 1 - Полностью выполнено

---
+++ src/main/java/arhangel/dim/container/Application.java
@@ -0,0 +1,10 @@
+package arhangel.dim.container;
+
+/**
+ * Created by spec45as on 3/23/2016.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Container Application");
+    }
+}
---
+++ src/main/java/arhangel/dim/container/InvalidConfigurationException.java
@@ -0,0 +1,10 @@
+package arhangel.dim.container;
+
+/**
+ * Ошибка конфигурации
+ */
+public class InvalidConfigurationException extends Exception {
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+}
