51f8c9dd46073e19c6f5fd95676edc1c5235c281
Nikita Uvarov <dragonicthebest@gmail.com>
2016-03-25 16:19:07 +0300

    Implemented Bean, BeanGraph, BeanXmlReader, Container

--- src/main/java/arhangel/dim/container/BeanXmlReader.java
+++ src/main/java/arhangel/dim/container/BeanXmlReader.java
@@ -1,11 +1,25 @@
 package arhangel.dim.container;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  *
  */
 public class BeanXmlReader {
+    private static final String TAG_ROOT = "root";
     private static final String TAG_BEAN = "bean";
     private static final String TAG_PROPERTY = "property";
     private static final String ATTR_NAME = "name";
@@ -14,8 +28,114 @@ public class BeanXmlReader {
     private static final String ATTR_BEAN_ID = "id";
     private static final String ATTR_BEAN_CLASS = "class";
 
-    public List<Bean> parseBeans(String pathToFile) {
-        return null;
+    private String getMandatoryAttribute(Node node, String attribute) throws InvalidConfigurationException {
+        Node propertyNode = node.getAttributes().getNamedItem(attribute);
+        if (propertyNode == null) {
+            throw new InvalidConfigurationException("Missing mandatory attribute '" +
+                    attribute + "' of node '" + node.getNodeName() + "'.");
+        }
+
+        String value = propertyNode.getNodeValue();
+        assert value != null;
+        return value;
+    }
+
+    private Property parseProperty(Node node) throws InvalidConfigurationException {
+
+        String value;
+        ValueType valueType;
+
+        Node valueAttr = node.getAttributes().getNamedItem(ATTR_VALUE);
+        Node refAttr = node.getAttributes().getNamedItem(ATTR_REF);
+
+        if ((valueAttr == null && refAttr == null) || (valueAttr != null && refAttr != null)) {
+            throw new InvalidConfigurationException("Property must have exactly one of '" +
+                    ATTR_VALUE + "' and '" + ATTR_REF + "' attributes.");
+        }
+
+        if (valueAttr != null) {
+            value = valueAttr.getNodeValue();
+            valueType = ValueType.VAL;
+        } else {
+            value = refAttr.getNodeValue();
+            valueType = ValueType.REF;
+        }
+
+        String name = getMandatoryAttribute(node, ATTR_NAME);
+
+        assert value != null;
+        return new Property(name, value, valueType);
+    }
+
+    public List<Bean> parseBeans(String pathToFile) throws InvalidConfigurationException, IOException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        Document doc;
+
+        try {
+            builder = dbFactory.newDocumentBuilder();
+            doc = builder.parse(new FileInputStream(pathToFile));
+        } catch (SAXException e) {
+            throw new InvalidConfigurationException("Invalid XML file: " + e.getMessage());
+        } catch (ParserConfigurationException e) {
+            // can't do anything
+            throw new RuntimeException("Internal error: invalid XML parser configuration.");
+        }
+
+        NodeList nodes = doc.getChildNodes();
+        if (nodes.getLength() != 1 || !nodes.item(0).getNodeName().equals(TAG_ROOT)) {
+            throw new InvalidConfigurationException("Expected single '" + TAG_ROOT + "' node.");
+        }
+
+        Node root = nodes.item(0);
+        NodeList rootChildren = root.getChildNodes();
+
+        ArrayList<Bean> beans = new ArrayList<>();
+
+        for (int beanIndex = 0; beanIndex < rootChildren.getLength(); beanIndex++) {
+            // why isn't it iterable?
+            Node beanNode = rootChildren.item(beanIndex);
+
+            if (beanNode.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+
+            if (!beanNode.getNodeName().equals(TAG_BEAN)) {
+                throw new InvalidConfigurationException("Invalid '" + beanNode.getNodeName() +
+                        "' children of '" + TAG_ROOT + "'.");
+            }
+
+            NodeList properties = beanNode.getChildNodes();
+
+            String name = getMandatoryAttribute(beanNode, ATTR_BEAN_ID);
+            String className = getMandatoryAttribute(beanNode, ATTR_BEAN_CLASS);
+
+            Map<String, Property> beanProperties = new HashMap<>();
+
+            for (int propertyIndex = 0; propertyIndex < properties.getLength(); propertyIndex++) {
+                Node propertyNode = properties.item(propertyIndex);
+
+                if (propertyNode.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
+                }
+
+                if (!propertyNode.getNodeName().equals(TAG_PROPERTY)) {
+                    throw new InvalidConfigurationException("Invalid '" + propertyNode.getNodeName() +
+                            "' children of '" + TAG_BEAN + "'.");
+                }
+
+                Property property = parseProperty(propertyNode);
+                Property old = beanProperties.put(property.getName(), property);
+                if (old != null) {
+                    throw new InvalidConfigurationException("Duplicate property '" + property.getName() + "'.");
+                }
+            }
+
+            Bean bean = new Bean(name, className, beanProperties);
+            beans.add(bean);
+        }
+
+        return beans;
     }
 
 }
--- src/main/java/arhangel/dim/container/Container.java
+++ src/main/java/arhangel/dim/container/Container.java
@@ -1,20 +1,48 @@
 package arhangel.dim.container;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 /**
  * Используйте ваш xml reader чтобы прочитать конфиг и получить список бинов
  */
 public class Container {
-    private List<Bean> beans;
+    private Map<String, Bean> beans = new HashMap<>();
+    private Map<Bean, Object> instantiatedBeans = new HashMap<>();
+    private Map<String, Bean> beanByClass = new HashMap<>();
 
     /**
      * Если не получается считать конфиг, то бросьте исключение
      * @throws InvalidConfigurationException неверный конфиг
      */
     public Container(String pathToConfig) throws InvalidConfigurationException {
+        BeanXmlReader reader = new BeanXmlReader();
+        List<Bean> beansList;
+        try {
+            beansList = reader.parseBeans(pathToConfig);
+        } catch (IOException e) {
+            // Tests contain catches for InvalidConfigurationException only -- why?
+            // I really should not wrap this
+            throw new InvalidConfigurationException("IOException: " + e.getMessage());
+        }
 
-        // вызываем BeanXmlReader
+        for (Bean bean: beansList) {
+            if (beans.put(bean.getName(), bean) != null) {
+                throw new InvalidConfigurationException("Duplicate bean name '" + bean.getName() + "'.");
+            }
+
+            beanByClass.put(bean.getClassName(), bean);
+        }
+
+        try {
+            instantiateBeans();
+        } catch (CycleReferenceException e) {
+            throw new InvalidConfigurationException(e.getMessage());
+        }
     }
 
     /**
@@ -22,43 +50,126 @@ public class Container {
      *  Например, Car car = (Car) container.getByName("carBean")
      */
     public Object getByName(String name) {
-        return null;
+        return instantiatedBeans.get(beans.get(name));
     }
 
     /**
      * Вернуть объект по имени класса
      * Например, Car car = (Car) container.getByClass("arhangel.dim.container.Car")
+     *
+     * Если объектов такого класса несколько, возвращает произвольный.
      */
     public Object getByClass(String className) {
-        return null;
+        return instantiatedBeans.get(beanByClass.get(className));
     }
 
-    private void instantiateBean(Bean bean) {
+    private void instantiateBeans() throws CycleReferenceException, InvalidConfigurationException {
+        BeanGraph graph = new BeanGraph();
 
-        /*
-        // Примерный ход работы
+        HashMap<Bean, BeanVertex> beanVertexMap = new HashMap<>();
+        for (Bean bean: beans.values()) {
+            beanVertexMap.put(bean, graph.addVertex(bean));
+        }
 
-        String className = bean.getClassName();
-        Class clazz = Class.forName(className);
-        // ищем дефолтный конструктор
-        Object ob = clazz.newInstance();
+        for (Bean bean: beans.values()) {
+            for (Property p : bean.getProperties().values()) {
+                if (p.getType() == ValueType.REF) {
+                    Bean refBean = beans.get(p.getValue());
+                    if (refBean == null) {
+                        throw new InvalidConfigurationException("Unresolved reference to '" + p.getValue() +
+                                "' of bean '" + bean.getName() + "'.");
+                    }
 
+                    graph.addEdge(beanVertexMap.get(bean), beanVertexMap.get(refBean));
+                }
+            }
+        }
 
-        for (String name : bean.getProperties().keySet()) {
-            // ищем поле с таким именен внутри класса
-            // учитывая приватные
-            Field field = clazz.getDeclaredField(name);
-            // проверяем, если такого поля нет, то кидаем InvalidConfigurationException с описание ошибки
+        List<BeanVertex> sorted = graph.sortBeans();
+        for (BeanVertex b: sorted) {
+            instantiateBean(b.getBean());
+        }
+    }
 
-            // Делаем приватные поля доступными
-            field.setAccessible(true);
+    private String makeFirstUpper(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 
-            // Далее определяем тип поля и заполняем его
-            // Если поле - примитив, то все просто
-            // Если поле ссылка, то эта ссылка должа была быть инициализирована ранее
+    private Object objectFromString(String str, Class desiredType) throws InvalidConfigurationException {
+        if (desiredType == int.class) {
+            return Integer.parseInt(str);
+        }
 
-            */
+        if (desiredType == String.class) {
+            return str;
+        }
 
+        throw new InvalidConfigurationException("Creation of '" + desiredType.getName() +
+                "' objects from string is not implemented.");
+    }
+
+    private void instantiateBean(Bean bean) throws InvalidConfigurationException {
+        Class beanClass;
+
+        try {
+            beanClass = Class.forName(bean.getClassName());
+        } catch (ClassNotFoundException e) {
+            throw new InvalidConfigurationException("Class '" + bean.getClassName() +
+                    "' of bean '" + bean.getName() + "' not found.");
+        }
+
+        try {
+            Object beanInstance = beanClass.newInstance();
+
+            for (String propertyName: bean.getProperties().keySet()) {
+                Property property = bean.getProperties().get(propertyName);
+                String setterName = "set" + makeFirstUpper(propertyName);
+
+                // search by hand because parameters are unknown
+                Method[] methods = beanClass.getMethods();
+                Method found = null;
+                for (Method m: methods) {
+                    if (m.getName().equals(setterName)) {
+                        if (found != null) {
+                            throw new InvalidConfigurationException("Multiple setters found in '" +
+                                    bean.getClassName() + "' for '" + propertyName + "'.");
+                        }
+                        found = m;
+                    }
+                }
+
+                if (found == null) {
+                    throw new InvalidConfigurationException("No setter '" + setterName +
+                            "' found in '" + bean.getClassName() + "'.");
+                }
+
+                switch (property.getType()) {
+                    case REF: {
+                        found.invoke(beanInstance, getByName(property.getValue()));
+                        break;
+                    }
+
+                    case VAL: {
+                        Parameter[] params = found.getParameters();
+                        if (params.length != 1) {
+                            throw new InvalidConfigurationException("Setter for '" + propertyName + "' in '" +
+                                    bean.getClassName() + "' must accept single parameter.");
+                        }
+
+                        Parameter param = params[0];
+                        Object value = objectFromString(property.getValue(), param.getType());
+                        found.invoke(beanInstance, value);
+                        break;
+                    }
+
+                    default: assert false;
+                }
+            }
+
+            instantiatedBeans.put(bean, beanInstance);
+        } catch (Exception e) {
+            throw new InvalidConfigurationException("Failed to instantiate bean: " + e.getMessage());
+        }
     }
 
 }
