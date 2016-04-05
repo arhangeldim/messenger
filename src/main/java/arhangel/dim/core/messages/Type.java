abb244aa1247a82b35e077bc9251ae4b122487fc
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2016-04-05 16:15:48 +0300

    Task: messenger

---
+++ src/main/java/arhangel/dim/core/messages/Type.java
@@ -0,0 +1,16 @@
+package arhangel.dim.core.messages;
+
+/**
+ * Маркер типа сообщения
+ */
+public enum Type {
+    MSG_LOGIN,
+    MSG_TEXT,
+    MSG_PASS,
+    MSG_HELP,
+    MSG_INFO,
+    MSG_CHAT_LIST,
+    MSG_CHAT_CREATE,
+    MSG_CHAT_HIST
+
+}
