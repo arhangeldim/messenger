5044e64aedcc627f70c5d919734be1e8583b899e
Rustam Kildiev <r.kildiev@corp.mail.ru>
2016-04-29 15:02:59 +0300

    nio

--- src/main/java/arhangel/dim/lections/threads/future/Sandbox.java
+++ src/main/java/arhangel/dim/lections/threads/future/Sandbox.java
@@ -1,22 +1,15 @@
 package arhangel.dim.lections.threads.future;
 
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 
 public class Sandbox {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         JFrame frame = buildFrame();
 
         frame.add(new ImagePanel());
@@ -30,23 +23,23 @@ public class Sandbox {
         return frame;
     }
 
-    static class ImagePanel extends JPanel {
+    private static class ImagePanel extends JPanel {
 
         private BufferedImage image;
 
-        public ImagePanel() throws Exception {
+        ImagePanel() throws Exception {
 
             image = ImageIO.read(new URL("https://gic6.mycdn.me/image?t=56&bid=816871160539&id=816871160539&plc=WEB&tkn=*iaoYC50VGni4tMfp7sTdPUMsBD0"));
-            Dimension d = new Dimension();
-            d.width = image.getWidth(null);
-            d.height = image.getHeight(null);
-            setPreferredSize(d);
+            Dimension dimension = new Dimension();
+            dimension.width = image.getWidth(null);
+            dimension.height = image.getHeight(null);
+            setPreferredSize(dimension);
         }
 
         @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2d = (Graphics2D) g;
+        public void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            Graphics2D g2d = (Graphics2D) graphics;
             g2d.drawImage(image, 0, 0, null);
         }
     }
